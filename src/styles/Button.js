ea6600a28e5c61c3a2f03fc86aa9e07f2e287a48
Xavier Hollingsworth <xavierhollingsworth@Xaviers-MacBook-Pro-2.local>
2020-03-05 15:32:28 -0500

    red

--- src/styles/Button.js
+++ src/styles/Button.js
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 import theme from './theme';
 const { colors, fontSizes, fonts } = theme;
 
-const Button = styled.button`
+const Button = styled.button `
   color: ${colors.green};
   background-color: transparent;
-  border: 1px solid ${colors.green};
+  border: 1px solid ${colors.red};
   border-radius: ${theme.borderRadius};
   font-size: ${fontSizes.smallish};
   font-family: ${fonts.SFMono};
